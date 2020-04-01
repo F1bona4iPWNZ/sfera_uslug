84ebb0369f4bc2eccef3ddf1df1b2f1bbe756cc0
Sasha_Gontar <gontar.199640@gmail.com>
2020-04-01 17:18:15 +0300

    about_part

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(function(){
 
+    $('.slider_container').slick({
+        nextArrow: '<button type="button" class="slick-button slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-button slick-prev"></button>'
+    });
+
+    $('select').styler()
 
 });
